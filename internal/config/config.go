ec0f86c9665aa798a759123a8ac30b752903ac5c
Murad <muradrmagomedov@gmail.com>
2025-05-26 21:33:16 +0300

    iter4

---
+++ internal/config/config.go
@@ -0,0 +1,18 @@
+package config
+
+import "flag"
+
+type Config struct {
+	Addr         string
+	ShortURLAddr string
+}
+
+func NewConfig() Config {
+	ctf := Config{}
+	Addr := flag.String("a", "localhost:8080", "host address")
+	ShortURLAddr := flag.String("b", "http://localhost:8000/", "prefix for short url")
+	flag.Parse()
+	ctf.Addr = *Addr
+	ctf.ShortURLAddr = *ShortURLAddr
+	return ctf
+}
