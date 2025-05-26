fe01248081b86ebb3b0b305e4dc5f0eb6c24fe3b
Murad <muradrmagomedov@gmail.com>
2025-05-26 23:03:21 +0300

    iter5

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,18 +1,27 @@
 package config
 
-import "flag"
+import (
+	"flag"
+
+	"github.com/caarlos0/env/v11"
+)
 
 type Config struct {
-	Addr         string
-	ShortURLAddr string
+	Addr         string `env:"SERVER_ADDRESS"`
+	ShortURLAddr string `env:"BASE_URL"`
 }
 
 func NewConfig() Config {
 	ctf := Config{}
+	env.Parse(&ctf)
 	Addr := flag.String("a", "localhost:8080", "host address")
 	ShortURLAddr := flag.String("b", "http://localhost:8080", "prefix for short url")
 	flag.Parse()
-	ctf.Addr = *Addr
-	ctf.ShortURLAddr = *ShortURLAddr
+	if ctf.Addr == "" {
+		ctf.Addr = *Addr
+	}
+	if ctf.ShortURLAddr == "" {
+		ctf.ShortURLAddr = *ShortURLAddr
+	}
 	return ctf
 }
